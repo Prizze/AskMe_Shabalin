e0ee876370f0e2b5542dbd6f70a806b948d91399
Maksim <sabalin291@gmail.com>
2024-11-13 10:39:36 +0300

    fill_bd fixed

--- project/AskMe/management/commands/fill_db.py
+++ project/AskMe/management/commands/fill_db.py
@@ -15,108 +15,100 @@ class Command(BaseCommand):
         ratio = options['ratio']
         self.stdout.write(f"Generate data with ratio = {ratio}")
 
-        # users = []
-        # profiles = []
-        # counter = 0
-        # base_username = "username"
-        # for _ in range(ratio):
-        #     logos = ['avatars/logo1.png', 'avatars/logo2.jpeg', 'avatars/logo3.jpeg']
-        #     random_logo = random.choice(logos)
+        users = []
+        profiles = []
+        counter = 0
+        base_username = "username"
+        for _ in range(ratio):
+            logos = ['avatars/logo1.png', 'avatars/logo2.jpeg', 'avatars/logo3.jpeg']
+            random_logo = random.choice(logos)
 
-        #     username = f"{base_username}{counter}"
+            username = f"{base_username}{counter}"
 
-        #     user = User(username=username, email=fake.email())
-        #     user.set_password('password')
-        #     users.append(user)
+            user = User(username=username, email=fake.email())
+            user.set_password('password')
+            users.append(user)
 
-        #     profile = Profile(user=user, avatar=random_logo, created_at=fake.date_time_this_year(), rating=0)
-        #     profiles.append(profile)
+            profile = Profile(user=user, avatar=random_logo, created_at=fake.date_time_this_year(), rating=0)
+            profiles.append(profile)
 
-        #     counter += 1
+            counter += 1
 
-        #     if (counter % 100 == 0):
-        #         self.stdout.write(f'Created {counter} users')
+            if (counter % 100 == 0):
+                self.stdout.write(f'Created {counter} users')
 
-        # User.objects.bulk_create(users)
-        # Profile.objects.bulk_create(profiles)
+        User.objects.bulk_create(users)
+        Profile.objects.bulk_create(profiles)
 
-        # self.stdout.write(self.style.SUCCESS(f'Created {len(users)} users and profiles.'))
+        self.stdout.write(self.style.SUCCESS(f'Created {len(users)} users and profiles.'))
 
-        users = User.objects.all()
-        profiles = Profile.objects.all()
+        base_tag = "tag_"
+        tags = []
+        counter = 0
+        while len(tags) < ratio:
+            tag_name = f"{base_tag}{counter}"
+            tags.append(Tag(name=tag_name))
+            counter += 1
+            if counter % 100 == 0:
+                self.stdout.write(f'Created {counter} tags')
 
-        # base_tag = "tag_"
-        # tags = []
-        # counter = 0
-        # while len(tags) < ratio:
-        #     tag_name = f"{base_tag}{counter}"
-        #     tags.append(Tag(name=tag_name))
-        #     counter += 1
-        #     if counter % 100 == 0:
-        #         self.stdout.write(f'Created {counter} tags')
-
-        # Tag.objects.bulk_create(tags)
-        # self.stdout.write(self.style.SUCCESS(f'Created {len(tags)} unique tags.'))
+        Tag.objects.bulk_create(tags)
+        self.stdout.write(self.style.SUCCESS(f'Created {len(tags)} unique tags.'))
         
-        #tags = Tag.objects.all()
-        # counter = 0
-        # questions = []
-        # for _ in range(ratio * 10):
-        #     profile = random.choice(users).profile
-        #     question = Question(
-        #         profile=profile,
-        #         title=fake.sentence(nb_words=6),
-        #         body=fake.paragraph(nb_sentences=3),
-        #         created_at=fake.date_time_this_year(),
-        #         rating=0,
-        #         like_count=0,
-        #         dislike_count=0,
-        #     )
-        #     questions.append(question)
+        counter = 0
+        questions = []
+        for _ in range(ratio * 10):
+            profile = random.choice(users).profile
+            question = Question(
+                profile=profile,
+                title=fake.sentence(nb_words=6),
+                body=fake.paragraph(nb_sentences=3),
+                created_at=fake.date_time_this_year(),
+                rating=0,
+                like_count=0,
+                dislike_count=0,
+            )
+            questions.append(question)
             
-        #     counter += 1
-        #     if counter % 1000 == 0:
-        #         self.stdout.write(f'Created {counter} questions')
+            counter += 1
+            if counter % 1000 == 0:
+                self.stdout.write(f'Created {counter} questions')
 
-        # Question.objects.bulk_create(questions)
-        # self.stdout.write(self.style.SUCCESS(f'Created {len(questions)} questions.'))
+        Question.objects.bulk_create(questions)
+        self.stdout.write(self.style.SUCCESS(f'Created {len(questions)} questions.'))
 
 
-        # for question in questions:
-        #     selected_tags = random.sample(tags, k=random.randint(1, 5))
-        #     question.tags.set(selected_tags)
-
-        questions = Question.objects.all()
+        for question in questions:
+            selected_tags = random.sample(tags, k=random.randint(1, 5))
+            question.tags.set(selected_tags)
         
-        # counter = 0
-        # answers = []
-        # for _ in range(ratio * 100):
-        #     profile = random.choice(users).profile
-        #     question = random.choice(questions)
-        #     answer = Answer(
-        #         profile=profile,
-        #         question=question,
-        #         body=fake.paragraph(nb_sentences=3),
-        #         created_at=fake.date_time_this_year(),
-        #         rating=0,
-        #         like_count=0,
-        #         dislike_count=0,
-        #         is_correct=random.choice([True, False]),
-        #     )
-        #     answers.append(answer)
+        counter = 0
+        answers = []
+        for _ in range(ratio * 100):
+            profile = random.choice(users).profile
+            question = random.choice(questions)
+            answer = Answer(
+                profile=profile,
+                question=question,
+                body=fake.paragraph(nb_sentences=3),
+                created_at=fake.date_time_this_year(),
+                rating=0,
+                like_count=0,
+                dislike_count=0,
+                is_correct=random.choice([True, False]),
+            )
+            answers.append(answer)
 
-        #     counter += 1
-        #     if counter % 10000 == 0:
-        #         self.stdout.write(f'Created {counter} asks')
+            counter += 1
+            if counter % 10000 == 0:
+                self.stdout.write(f'Created {counter} asks')
 
-        # Answer.objects.bulk_create(answers)
+        Answer.objects.bulk_create(answers)
 
-        # self.stdout.write(self.style.SUCCESS(f'Created {len(answers)} answers.'))
+        self.stdout.write(self.style.SUCCESS(f'Created {len(answers)} answers.'))
 
-        answers = Answer.objects.all()
 
         counter = 0
-        
         existing_question_reactions = set()
         existing_answer_reactions = set()
         for _ in range(ratio * 200):
